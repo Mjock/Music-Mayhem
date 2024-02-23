06098f0a933503f134add6152dc319e6086abc93
Mjock <92009330+Mjock@users.noreply.github.com>
2024-02-23 23:21:18 +0530

    last commit

--- Music Mayhem/src/main/java/com/main/project/repositories/PlaylistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.main.project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.main.project.entities.Playlist;
-
-public interface PlaylistRepository extends JpaRepository<Playlist,Integer> {
-
-}
